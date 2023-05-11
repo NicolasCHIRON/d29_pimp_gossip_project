0c9a1c2ccce79eae4abb0c0abd6ffb43cb64141c
jace <nic.chiron@gmail.com>
2023-05-11 14:34:11 +0200

    create sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    puts params
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Bienvenue parmi nous, #{@user.first_name}"
+      redirect_to '/'
+    else
+      flash[:alert] = 'Erreur de saisie, veuillez recommencer'
+      render 'new'
+    end
+
+  end
+
+  def destroy
+    @session.delete(:user_id)
+  end
+end
